1739b129cec5522c34ecac533d759c1584e663bb
Olivier Mungo <olivier@mungo.eu>
2023-04-16 00:21:08 +0200

    .

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                'dark-space': 'rgba(2, 0, 36, 1)',
+            },
+        },
+    },
+    plugins: [],
+};
